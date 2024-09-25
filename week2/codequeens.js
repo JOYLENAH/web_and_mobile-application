e89a8d230a9ca9bf3492685bfa3108953d0587b3
JOYLENAH <babiryejoylenah@gmail.com>
2024-09-25 23:01:44 +0300

    codequeens_work.js
    learning how to create variables for work given and creating functions

---
+++ week2/codequeens.js
@@ -0,0 +1,31 @@
+let queen_name="joy"
+let completed_challenges=0;
+let increment=completedchallenges +1;
+function celebratewin(){
+    console.log("you did it queen")
+}
+celebratewin();
+
+function celebratewin(challengename){
+    console.log(`challenge${challengename}completed!`)
+    console.log("time to take on the next challenge")
+}
+celebratewin("joy");
+
+for(let x=1; x<=3;x++){
+    console.log(`challenge${x}solved`);
+}
+
+for (let x=1; x<=3;x++){
+    console.log(`the tech queens solved challenges${x}`)
+}
+
+let nextchallenge="loops"
+if(nextchallenge="loops"){
+    console.log(`time to practice loops!`)
+}else if(nextchallenge="loops"){
+    console.log(`on the next challenge!`)
+}else(nextchallenge="functions"){
+    console.log(`time to master functions`)
+}
+
