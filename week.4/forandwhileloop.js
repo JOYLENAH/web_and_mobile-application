c947f76ee2f09efb8d9698c8c65516b6c9733177
JOYLENAH <babiryejoylenah@gmail.com>
2024-09-26 23:40:10 +0300

    ifstatments
    defining the functions and using the if statments

---
+++ week.4/forandwhileloop.js
@@ -0,0 +1,18 @@
+//for loop reapeats the code a spefic number of times 
+//a program that prints 1 to 10
+for (let i=1;i<=10;i++);{
+    console.log(i)
+}
+
+//multiplication of 5
+
+let i = 1;
+ while (i <= 5) {
+ console.log(i);
+ i++
+ }
+
+
+
+
+    
---
+++ week.4/ifstatments.js
@@ -0,0 +1,14 @@
+let age =18;
+if(age>=18){
+    console.log(`you are eligible to vote`)
+}
+
+let score =100;
+if(score>=80){
+    console.log(`student gets an A`)
+}else if( score>60 && score<79){
+    console.log(`student gets a B`)
+}else{
+    console.log(`they need to improve`)
+}
+
