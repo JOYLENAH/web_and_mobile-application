56de67b98bc8e0f769b094a922382bd3f94d77c6
JOYLENAH <babiryejoylenah@gmail.com>
2024-09-28 08:20:50 +0300

    practicing loops
    learning how to use  the for loops, while and do loops in the work

--- week.4/forandwhileloop.js
+++ week.4/forandwhileloop.js
@@ -4,13 +4,56 @@ for (let i=1;i<=10;i++);{
     console.log(i)
 }
 
-//multiplication of 5
+//printing multiplication table for 1-10
+const num=5;
+for(let i=1;i<=12;i++){
+    console.log(`${num} x ${i}=${num*i}`)
+}
 
-let i = 1;
- while (i <= 5) {
- console.log(i);
- i++
+
+//a while loop for the sum from 1 - 10
+//let i=1;
+//while(i=1 ; sum i<=110)
+ let sum_of_numbers =0;
+let number=1;s
+while (number <=10;i++);{
+    sum_of_numbers+=number;
+    number+=1;
+}console.log(`the sum of numbers from 1 to 10`,sum_of_numbers i++)
+
+// a do loop
+let i=1;
+do{
+    console.log(i);
+    i++;
+}while(i<=5);
+
+
+//incase of breaking
+for(i=1;i <=10;i++);{
+  if (i==5);{
+    break;// stops the loop when i=5
+  }console.log(i)
+  
+}
+ for(i=1;i<=10;i++);{
+    if(i==5);{
+        continue;//continues the iteration when i=5
+    }console.log(i)
  }
+
+ // writing a loop that reaches 7 then breaks
+ for(i=1;i<=10;i++);{
+    if(i==7);{
+        break;
+    }console.log(i)
+ }
+ //writing a program that prints numbers from 1to10 but skips even numbersusing continue
+ for(let number=1;number<=10;number++);{
+    if(number%2==0){
+        continue;
+    }console.log(number);
+}
 
 
 
