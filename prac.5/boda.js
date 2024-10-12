5ab01c47878acec188eb500bf7d1ad5c02d8952a
JOYLENAH <babiryejoylenah@gmail.com>
2024-10-12 23:38:11 +0300

    void
    learning

---
+++ prac.5/boda.js
@@ -0,0 +1,13 @@
+//an object representing boda boda
+function bodarider(name,location,bodatype){
+     this.bodaname=name;
+     this.bodalocation=location;
+     this.bodatype=bodatype;
+     this.introduce=function(){
+          return bodarider
+     }
+
+}
+
+let bodarider1= new bodarider("james","kireka","hit and run");
+console.log(bodarider);
